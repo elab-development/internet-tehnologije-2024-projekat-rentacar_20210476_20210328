40c9fa71771b5fc1cdffa95c228985f8f3202c99
Bogdan Terzic <boki.t2002.12@gmail.com>
2024-12-20 19:37:25 +0100

    Dodat User Controller

--- rent_a_car/routes/api.php
+++ rent_a_car/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']); 
 Route::post('/login', [AuthController::class, 'login']); 
@@ -8,6 +9,9 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/users', [UserController::class, 'index']); 
+    Route::get('/users/{id}', [UserController::class, 'show']); 
 
 
     Route::post('/logout', [AuthController::class, 'logout']);
