7980b91b30fdae29245d2f57a576dc3e2a55eed2
Bogdan Terzic <boki.t2002.12@gmail.com>
2024-12-20 19:19:48 +0100

    Uvedeni kontroler i resurs za Autentifikaciju

--- rent_a_car/database/migrations/2024_12_20_133010_add_personal_documents_column_into_users_table.php
+++ rent_a_car/database/migrations/2024_12_20_133010_add_personal_documents_column_into_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('personal_id', 500)->unique()->after('email'); 
-            $table->string('drivers_licence', 500)->nullable()->after('personal_id');
+            $table->text('personal_id', 500)->after('email'); 
+            $table->text('drivers_licence', 500)->after('personal_id');
         });
     }
 
