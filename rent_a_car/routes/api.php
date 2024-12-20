d1ce57fe9ab7fee2eac0f19f5e4be02a60b00985
Bogdan Terzic <boki.t2002.12@gmail.com>
2024-12-20 19:44:19 +0100

    Kreirani Rent kontroleri i resursi kao i rute

--- rent_a_car/routes/api.php
+++ rent_a_car/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RentController;
 
 Route::post('/register', [AuthController::class, 'register']); 
 Route::post('/login', [AuthController::class, 'login']); 
@@ -12,6 +13,11 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/users', [UserController::class, 'index']); 
     Route::get('/users/{id}', [UserController::class, 'show']); 
+
+    Route::get('/rents', [RentController::class, 'index']);
+    Route::delete('/rents/{id}', [RentController::class, 'destroy']); 
+    Route::post('/rents', [RentController::class, 'store']); 
+    Route::patch('/rents/{id}', [RentController::class, 'update']); 
 
 
     Route::post('/logout', [AuthController::class, 'logout']);
