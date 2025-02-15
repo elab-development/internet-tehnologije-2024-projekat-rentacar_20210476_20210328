f4bd6373e3e2d4ce95918c5011a8893a29c7e938
Sofija <sofija.filipovicc@gmail.com>
2025-02-15 14:53:09 +0100

    Namesten pocetni dizajn i boje koje ce se koristiti

--- rent_a_car_front/src/App.js
+++ rent_a_car_front/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> cao</h1>
+     <h2> buyee</h2>
+     <h3>didid</h3>
+     <p> hehhee</p>
+     <button> klik</button>
     </div>
   );
 }
