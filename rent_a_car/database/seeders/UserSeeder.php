fe2bd929ee0bb057adcfc06c44f12d461949ee85
Sofija <sofija.filipovicc@gmail.com>
2025-02-15 18:00:39 +0100

    Dodat logo i sitna izmena na backend seederu

--- rent_a_car/database/seeders/UserSeeder.php
+++ rent_a_car/database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'Admin User',
-            'email' => 'admin',
+            'email' => 'admin@gmail.com',
             'password' => bcrypt('godmode123'),
             'personal_id' => '',
             'drivers_licence' => '',
