faaa9ad02ed1329d0b3b8fe63c3eefda6a1de730
Sofija <sofija.filipovicc@gmail.com>
2025-02-15 19:16:37 +0100

    Dashboard i home kao 2 pocetne stranice za korisnike

--- rent_a_car_front/src/components/Home.jsx
+++ rent_a_car_front/src/components/Home.jsx
@@ -1,12 +1,53 @@
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+import Footer from "./Footer";
 
 const Home = ({ userData }) => {
-    return (
-      <div>
-        <h1>Dobrodošli, {userData.name}!</h1>
-        <p>Ovo je stranica za regular korisnike.</p>
-      </div>
-    );
+  const navigate = useNavigate();
+
+  const navigateToMyRents = () => {
+    navigate("/myrents");
   };
-  
-  export default Home;
+
+  const navigateToCars = () => {
+    navigate("/cars");
+  };
+
+
+  return (
+    <div className="home-page">
+        <div className="header"> 
+            <h1>Zdravo {userData.name}!</h1>
+            <div className="about-content">
+                <h2>O Rent a Car Aplikaciji</h2>
+                <p>
+                    Rent a Car je usluga koja omogućava jednostavno i brzo iznajmljivanje
+                    automobila. Bez obzira na to da li vam treba auto na par dana ili
+                    duže, mi imamo rešenje za vas. Naša ponuda uključuje različite
+                    modele automobila, od malih ekoloških vozila do luksuznih automobila.
+                </p>
+            </div>
+        </div>
+       
+      <section className="cards">
+        <div className="card">
+          <h3>Moja Rentiranja</h3>
+          <p>Pogledajte svoja prethodna i aktuelna rentiranja.</p>
+          <Button text="Pogledaj rentiranja" onClick={navigateToMyRents} />
+        </div>
+        <div className="card">
+          <h3>Ponuda Automobila</h3>
+          <p>Pogledajte sve automobile koje možete iznajmiti.</p>
+          <Button text="Pogledaj ponudu" onClick={navigateToCars} />
+        </div>
+      </section>
+
+    
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
